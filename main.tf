58281a5bc11eb54b20f2578a352d7d62817c2491
pandu-bolem <pandubolem67@gmail.com>
2026-04-01 13:14:30 +0000

    Initial Terraform code

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+ alias = "useast1"
+ region = "us-east-1"
+}
+provider "aws" {
+ alias = "uswest1"
+ region = "us-west-1"
+}
+variable "ami_value" {
+description = "value of ami "
+}
+variable "instance_type_value" {
+description = "value of instance "
+}
+variable "subnet_id_value"{
+description = " value of subnet"
+}
+variable "bucket_value"{
+description = "name of bucket "
+}
+module "aws_s3_bucket"  {
+source = "/home/ubuntu/terraform_project/terraform_modules/s3_bucket/"
+bucket_value = var.bucket_value
+providers = {
+aws = aws.uswest1
+}
+}
+module "aws_ec2_instance"{
+ source = "/home/ubuntu/terraform_project/terraform_modules/ec2_instance/"
+ ami_value = var.ami_value
+ instance_type_value = var.instance_type_value
+ subnet_id_value = var.subnet_id_value
+ providers = {
+ aws = aws.useast1
+}
+ }
+
+
+
+
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+ bucket_value = "cybersecurity-123456789"
+ ami_value = "ami-0f3caa1cf4417e51b"
+ instance_type_value = "t3.micro"
+ subnet_id_value = "subnet-0fe46811f8ea8f630"
+
